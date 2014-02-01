356c050527e8ec236091880058ac8684edf070e9
Leo Singer <leo.singer@ligo.org>
2014-02-01 14:20:05 -0800

    More documentation comments

--- gcn/__init__.py
+++ gcn/__init__.py
@@ -15,7 +15,21 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 """
-Anonymous VOEvent client for receiving GCNs in XML format.
+Anonymous VOEvent client for receiving GCN/TAN notices in XML format
+
+The Gamma-ray Coordinates Network/Transient Astronomy Network (GCN/TAN,
+http://gcn.gsfc.nasa.gov) is a system for distributing astronomical alerts,
+largely focused on operations of and detections from high-energy satellite
+missions.
+
+GCN/TAN disseminates both Notices (prompt, machine-readable alerts) and
+Circulars (human-readable correspondence) through a handful of delivery methods
+and formats.
+
+This package implements a simple client that anonymously listens for VOEvent
+(http://www.ivoa.net/documents/VOEvent) XML format notices over the custom
+TCP/IP VOEvent Transport Protocol
+(http://www.ivoa.net/documents/Notes/VOEventTransport).
 """
 __author__ = "Leo Singer <leo.singer@ligo.org>"
 
