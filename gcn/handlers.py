7a3759d729aa810adf78f89beb9834e9978a87cd
Leo Singer <leo.singer@ligo.org>
2014-02-01 13:59:40 -0800

    Update copyright date

--- gcn/handlers.py
+++ gcn/handlers.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Leo Singer
+# Copyright (C) 2014  Leo Singer
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
