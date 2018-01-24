718db875f9276774837bbc883e8f2ed6a0fb0b2c
Leo Singer <leo.singer@ligo.org>
2018-01-24 10:07:23 -0500

    Clean up imports

--- gcn/voeventclient.py
+++ gcn/voeventclient.py
@@ -32,12 +32,8 @@ try:
         return lxml.etree.parse(io.BytesIO(text)).getroot()
     from lxml.etree import XMLSyntaxError
 except ImportError:
-    import xml.etree.cElementTree
-    parse_from_string = xml.etree.cElementTree.fromstring
-    try:
-        from xml.etree.cElementTree import ParseError as XMLSyntaxError
-    except ImportError: # Python 2.6 raises a different exception
-        from xml.parsers.expat import ExpatError as XMLSyntaxError
+    from xml.etree.cElementTree import fromstring as parse_from_string
+    from xml.etree.cElementTree import ParseError as XMLSyntaxError
 import logging
 import datetime
 import base64
